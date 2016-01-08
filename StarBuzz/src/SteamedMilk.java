d488f57ca1aafdf2012433f4346d1a7a04102152
Mengqi Zong <mzong@walmartlabs.com>
2016-01-08 09:18:41 -0800

    Chapter 3: Star Buzz Coffee

---
+++ StarBuzz/src/SteamedMilk.java
@@ -0,0 +1,18 @@
+
+public class SteamedMilk extends CondimentDecorator {
+    Beverage beverage;
+
+    public SteamedMilk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Steamed Milk";
+    }
+
+    @Override
+    public double cost() {
+        return .10 + beverage.cost();
+    }
+}
